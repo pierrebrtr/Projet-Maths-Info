68912e93465a34a1cf2f241aafd1c946778d0539
Pierre Bertier <pierredelaval@hotmail.fr>
2019-05-05 15:51:15 +0200

    Tri-coloration
    Work in progress tri-coloration

--- projet.py
+++ projet.py
@@ -1,15 +1,24 @@
 from tkinter import *
 from math import *
+import time
+
+print("""
+      
+ _____ ___ _  __  __  _  __ _   ___  
+|_   _| _ \ |/  \|  \| |/ _] | | __| 
+  | | | v / | /\ | | ' | [/\ |_| _|  
+  |_| |_|_\_|_||_|_|\__|\__/___|___| 
+
+""")
 
 points = []
-triliste = []
+global triliste
 
 
 def demarrer(w = 500, h = 500):
     global width
     global height
     global main
-
 
     width = w
     height = h
@@ -23,10 +32,6 @@ def demarrer(w = 500, h = 500):
 
     return
 
-
-
-
-
 class Application:
 
     def __init__(self):
@@ -34,7 +39,14 @@ class Application:
         self.fen = Tk()
         self.fen.title("Projet Maths Info | Paul & Pierre")
 
-        
+##        self.menubar = Frame(self.fen)
+##        self.menubar.pack(side="top",fill=X)
+##        self.fbutton = Menubutton(self.menubar,text="Menu",underline = 0)
+##        self.fbutton.pack(side="left")
+##
+##        self.filemenu = Menu(self.fbutton)
+##        self.filemenu.add_command(label="Quit",command = self.fen.destroy)
+##        self.fbutton.config(menu=self.filemenu)
 
         self.canvas = Canvas(self.fen, bg="white", width=width, height= height)
         self.canvas.configure(cursor="crosshair")
@@ -48,14 +60,20 @@ class Application:
         ##,ipady=100
         
 
-        self.bouton_trianguler = Button(self.frame, text="Trianguler", bg = "grey",command=triangulerf)
+        self.bouton_trianguler = Button(self.frame, text="Trianguler", bg = "grey",command=trianguler)
         self.bouton_trianguler.grid(row=2,column =1,pady=10)
 
-        self.bouton_trianguler = Button(self.frame, text="Effacer", bg = "grey",command=self.clear)
-        self.bouton_trianguler.grid(row=3,column =1,pady=10)
+        self.bouton_coloration = Button(self.frame, text="Coloration", bg = "blue",command=coloration)
+        self.bouton_coloration.grid(row=4,column =1,pady=10)
 
-        self.bouton_trianguler = Button(self.frame, text="Quitter", bg = "red",command=self.fen.destroy)
-        self.bouton_trianguler.grid(row=4,column =1,pady=10)
+        self.bouton_clear = Button(self.frame, text="Effacer", bg = "grey",command=self.clear)
+        self.bouton_clear.grid(row=5,column =1,pady=10)
+
+        self.bouton_quitter = Button(self.frame, text="Quitter", bg = "red",command=self.fen.destroy)
+        self.bouton_quitter.grid(row=6,column =1,pady=10)
+
+        self.bouton_chiffre = Button(self.frame, text="Chiffres", bg = "green",command=chiffre)
+        self.bouton_chiffre.grid(row=3,column =1,pady=10)
 
     def clear(self):
         points.clear()
@@ -63,11 +81,6 @@ class Application:
         main.canvas.bind("<Motion>",move)
         main.canvas.bind("<Button-1>", point)
     
-
-
-
-        
-
 
 def point(event):
     points.append((event.x,event.y))
@@ -173,22 +186,161 @@ def triangulationb(points,triliste):
             triangulationb(polybb,triliste)
     return triliste
 
-def trianguler(points):
+def triangulerbis(points):
     liste = []
     triangulationb(points,liste)
     return liste
 
 def drawT(liste):
-    print(liste)
     for triangle in liste:
-        print(triangle)
         main.canvas.create_line(triangle[0][0],triangle[0][1],triangle[1][0],triangle[1][1], tags="triangl", width= 1)
         main.canvas.create_line(triangle[0][0],triangle[0][1],triangle[2][0],triangle[2][1], tags="triangl", width= 1)
         main.canvas.create_line(triangle[1][0],triangle[1][1],triangle[2][0],triangle[2][1], tags="triangl", width= 1)
 
-def triangulerf():
+def trianguler():
+    global triliste
     if len(points)>=4:
-        drawT(trianguler(points))
+        triliste = triangulerbis(points)
+        drawT(triliste)
+
+
+
+
+
+def chiffre():
+    global triliste
+    if (len(points)>=4):
+        liste = triliste
+        for i in range(len(liste)):
+            x = (liste[i][0][0]+liste[i][1][0]+liste[i][2][0])/3
+            y = (liste[i][0][1]+liste[i][1][1]+liste[i][2][1])/3
+            main.canvas.create_text(x,y,text=i)
+
+
+colors = ["blue","red","green"]
+
+##def testar(t1,t2):
+##    colorsb = ["blue","red","green"]
+##    success = False
+##    pos = 0
+##    p1 = t1[0]
+##    p2 = t1[1]
+##    p3 = t1[2]
+##    main.canvas.create_text(p1[0],p1[1],text="1")
+##    main.canvas.create_text(p2[0],p2[1],text="2")
+##    main.canvas.create_text(p3[0],p3[1],text="3")
+##    p1b = t2[0]
+##    p2b = t2[1]
+##    p3b = t2[2]
+##    i1 = -1
+##    i2 = -1
+##    i3=-1
+##    try:
+##        i1 = t1.index(p1b)
+##        try:
+##            i2 = t1.index(p2b)
+##            success = True
+##            pos = 3
+##        except:
+##            try:
+##                i3 = t1.index(p3b)
+##                success = True
+##                pos = 2
+##            except:
+##                success = False
+##    except:
+##        try:
+##            i2 = t1.index(p2b)
+##            try:
+##                i1 = t1.index(p1b)
+##                success = True
+##                pos = 3
+##            except:
+##                try:
+##                    i3 = t1.index(p3b)
+##                    success = True
+##                    pos = 1
+##                except:
+##                    success = False
+##        except: 
+##            try:
+##                i3 = t1.index(p3b)
+##                try:
+##                    i1 = t1.index(p1b)
+##                    success = True
+##                    pos = 2
+##                except:
+##                    try:
+##                        i2 = t1.index(p2b)
+##                        success = True
+##                        pos = 1
+##                    except:
+##                        success = False
+##            except:
+##                print("")
+##    if success :
+##        if (i1 != -1):
+##            colorsb.remove(main.canvas.itemcget(main.canvas.find_withtag(str(t1[i1][0])+","+str(t1[i1][1])), "fill"))
+##        if (i2 != -1):
+##            colorsb.remove(main.canvas.itemcget(main.canvas.find_withtag(str(t1[i2][0])+","+str(t1[i2][1])), "fill"))
+##        if (i3 != -1):
+##            colorsb.remove(main.canvas.itemcget(main.canvas.find_withtag(str(t1[i3][0])+","+str(t1[i3][1])), "fill"))
+##    else :
+##        colorsb = ["grey"]
+##    return (success,pos - 1,colorsb[0])      
+##
+##
+##
+
+
+def segmentation(t1,t2):
+    success = False
+    if((t1[0] in t2 and t1[1] in t2)  or (t1[2] in t2 and t1[1] in t2) or (t1[0] in t2 and t1[2] in t2)):
+        success = True
+        
+    return (success)
+
+
+global bliste
+
+def coloration():
+    colors = ["red","green","blue"]
+    global bliste,triliste
+    bliste = triliste
+    print("------")
+    print("Triangle 1")
+    for j in range (0,3):
+        main.canvas.create_oval(bliste[0][j][0] - 8, bliste[0][j][1] - 8, bliste[0][j][0]+8,bliste[0][j][1]+8, fill=colors[j],tags=str(bliste[0][j][0])+","+str(bliste[0][j][1]),width="2")
+        print("Point n°" + str(j + 1) + " : ",main.canvas.itemcget(main.canvas.find_withtag(str(bliste[0][j][0])+","+str(bliste[0][j][1])), "fill"),str(bliste[0][j][0])+","+str(bliste[0][j][1]))
+    index = 0
+    print("------")
+    recur(0)
+            
+def recur(index):
+    global bliste
+    if len(bliste) > 1:
+        st = []
+        for x in range(len(bliste)):
+            if bliste[x] != "N":
+                if (index != x):
+                    if(segmentation(bliste[index],bliste[x])):
+                        st.append(bliste[x])
+                        print("Triangle n°" + str(x) + " collé à triangle n°" + str(index))
+                        triangle = bliste[x]
+                        colors = ["red","green","blue"]
+                        id = [0,1,2]
+                        for i in range (len(triangle)):
+                            if (triangle[i] in bliste[index]):
+                                colors.remove(main.canvas.itemcget(main.canvas.find_withtag(str(triangle[i][0])+","+str(triangle[i][1])), "fill"))
+                                id.remove(i)
+                        y = id[0]
+                        main.canvas.create_oval( triangle[y][0]- 8, triangle[y][1] - 8, triangle[y][0]+8,triangle[y][1]+8, fill=colors[0],tags=str(triangle[y][0])+","+str(triangle[y][1]),width="2")
+                        bliste[index] = "N"
+                        print("NEXT ->", str(x))
+                        recur(x)
+
+
+
 
 
 demarrer()
