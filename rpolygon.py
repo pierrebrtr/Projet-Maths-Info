2647addb9d91338add61a756f6fbd1c73294605f
Pierre Bertier <pierredelaval@hotmail.fr>
2019-05-27 21:55:51 +0200

    Clean

--- rpolygon.py
+++ rpolygon.py
@@ -2,8 +2,6 @@ from math import *
 import math
 import random
 
-
-
 class GPoly():
 
     def __init__(self,n):
@@ -15,7 +13,6 @@ class GPoly():
         for i in range(n):
             x.append(random.randint(1,500 -1))
             y.append(random.randint(1,500 -1))
-
         xc = 0
         yc = 0
         for i in range(n):
@@ -27,19 +24,11 @@ class GPoly():
         angles = []
         for i in range(n):
             angles.append(math.atan2(x[i]-center[0],y[i] - center[1]))
-        ##sorting the points:
         sort_tups = sorted([(i,j,k) for i,j,k in zip(x,y,angles)], key = lambda t: t[2])
-
-        ##making sure that there are no duplicates:
-        if len(sort_tups) != len(set(sort_tups)):
-            raise Exception('two equal coordinates -- exiting')
-
         x,y,angles = zip(*sort_tups)
         x = list(x)
         y = list(y)
-
         m = self.merge(x,y)
-        print("SAVE",m)
         return m
 
     def merge(self,list1, list2):
